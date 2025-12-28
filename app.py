0fb643f673f0c39eed07f7ebfdcd4d112f628ecc
maruthi1225 <maruthi.nayak12345@gmail.com>
2025-12-28 14:23:24 +0530

    #

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 print("Hello World!")
 print("This is a sample Python application.")
 print("It demonstrates basic print functionality.")
-<<<<<<< HEAD
 print("Goodbye World!")
-=======
-printf("Hai Inturnstream!")
->>>>>>> e58d15a2f52fddca2fa9ec9caf8aad3dedd5a5a1
+print("Hai Inturnstream!")
+
